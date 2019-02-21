545a60e7eb9678cf6bd3073477923313d52ea271
DumunVolodymyr <dumunvova@gmail.com>
2019-02-21 09:08:57 +0200

    corrected campingProduct

--- src/ua/lviv/iot/camping/main.java
+++ src/ua/lviv/iot/camping/main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+import ua.lviv.iot.camping.manager.CampingManager;
 import ua.lviv.iot.camping.models.*;
 import ua.lviv.iot.camping.models.Backpack.*;
 import ua.lviv.iot.camping.models.SleepingBag.*;
@@ -11,41 +12,21 @@ import ua.lviv.iot.camping.models.Tent.*;
 
 public class main {
 	public static void main(String[] args) {
-		ArrayList<CampingProduct> cps = new ArrayList<CampingProduct>();
-		cps.add(new Tent(500, 20, "Reebok", 4, Construction.ONE_STORY, TentForm.LODGE));
-		cps.add(new Backpack(500, 15, "Adidas", 40, BackpackType.BAG, Color.RED));
-		cps.add(new SleepingBag(500, 30, "Aer", "Blanket", 2, SleepingBagMaterial.WOOL));
+		CampingManager cm = new CampingManager();
+		cm.addCampingProduct(new Tent(500, 20, "Reebok", 4, Construction.ONE_STORY, TentForm.LODGE));
+		cm.addCampingProduct(new Backpack(500, 15, "Adidas", 40, BackpackType.BAG, Color.RED));
+		cm.addCampingProduct(new SleepingBag(500, 30, "Aer", "Blanket", 2, SleepingBagMaterial.WOOL));
 		
 		System.out.println("CP 1");
-		for (CampingProduct campingProduct : cps) {
-			campingProduct.Show();
-		}
-		
-		Collections.sort(cps, new Comparator<CampingProduct>() {
-			@Override
-			public int compare(CampingProduct cp1, CampingProduct cp2) {
-				return Double.compare(cp1.getWeight(), cp2.getWeight());
-			}
-		});
+		cm.ShowCampingProducts();
 		
 		System.out.println("CP 2");
-		for (CampingProduct campingProduct : cps) {
-			campingProduct.Show();
-		}
-		
-		Collections.sort(cps, new Comparator<CampingProduct>() {
-			@Override
-			public int compare(CampingProduct cp1, CampingProduct cp2) {
-				return cp1.getProducerName().compareTo(cp2.getProducerName());
-			}
-		});
-		cps.sort((cp1, cp2) -> cp1.getProducerName().compareTo(cp2.getProducerName()));
+		cm.sortCampingProductByWeight(true);
+		cm.ShowCampingProducts();
 		
 		System.out.println("CP 3");
-		for (CampingProduct campingProduct : cps) {
-			campingProduct.Show();
-		}
-		
+		cm.sortCampingProductByProducerName();
+		cm.ShowCampingProducts();
 	}
 
 }
--- src/ua/lviv/iot/camping/manager/CampingManager.java
+++ src/ua/lviv/iot/camping/manager/CampingManager.java
@@ -1,6 +1,7 @@
 package ua.lviv.iot.camping.manager;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
 import ua.lviv.iot.camping.models.CampingProduct;
@@ -26,10 +27,34 @@ public class CampingManager implements Comparator<CampingProduct>{
 		products.sort(Comparator.comparing(CampingProduct::getPrice));
 		return this.products;
 	}
+	
+	public void sortCampingProductByWeight() {
+		products.sort(Comparator.comparing(CampingProduct::getWeight));
+	}
+	
+	public void sortCampingProductByWeight(boolean reverse) {
+		products.sort(Comparator.comparing(CampingProduct::getWeight));
+		if(reverse)
+			Collections.reverse(products);
+	}
+	
+	public void sortCampingProductByProducerName() {
+		products.sort(Comparator.comparing(CampingProduct::getProducerName));
+	}
+	
+	public void sortCampingProductByProducerName(boolean reverse) {
+		products.sort(Comparator.comparing(CampingProduct::getProducerName));
+		if(reverse)
+			Collections.reverse(products);
+	}
 
 	@Override
 	public int compare(CampingProduct cp1, CampingProduct cp2) {
 		return Double.compare(cp1.getPrice(), cp2.getPrice());
+	}
+	
+	public void addCampingProduct(CampingProduct cp) {
+		products.add(cp);
 	}
 }
 
